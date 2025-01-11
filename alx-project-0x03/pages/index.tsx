92dfea0a7e135f98214407e45e8074130581e542
kamaufnjeri <kamaufnjeri2019@gmail.com>
2025-01-11 22:02:12 +0300

    Task 0 - 4 added

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -13,6 +13,9 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+      <h1>
+        Welcome to Splash App!
+      </h1>
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
         Welcome to Splash App!
       </h1>
